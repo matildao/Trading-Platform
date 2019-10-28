8ac4998db781262501ec5d16390afaf70927bc19
Matilda <matildaytc@gmail.com>
2019-10-28 15:36:26 +0100

    Added socket.io

--- src/main.js
+++ src/main.js
@@ -3,8 +3,19 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router';
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import VueSocketIO from 'vue-socket.io'
 
 Vue.config.productionTip = false
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: process.env.VUE_APP_SOCKET_BASE,
+  vuex: {
+    actionPrefix: 'SOCKET_',
+    mutationPrefix: 'SOCKET_'
+  }
+}))
+
 
 new Vue({
   vuetify,
